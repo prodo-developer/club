ba7b48de49553dd6f2c91889ff772da84d539e2e
prodo-developer <wjdals300@gmail.com>
2021-09-02 01:31:14 +0900

    refactor : 헤더 검증 실패처리

--- src/main/java/org/zerock/club/security/filter/ApiCheckFilter.java
+++ src/main/java/org/zerock/club/security/filter/ApiCheckFilter.java
@@ -1,7 +1,9 @@
 package org.zerock.club.security.filter;
 
 import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.JSONObject;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -9,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Slf4j
 public class ApiCheckFilter extends OncePerRequestFilter {
@@ -41,9 +44,47 @@ public class ApiCheckFilter extends OncePerRequestFilter {
             log.info("ApiCheckFilter..............................");
             log.info("ApiCheckFilter..............................");
 
-            return;
+            boolean checkHeader = checkAuthHeader(request);
+
+            if(checkHeader) {
+                filterChain.doFilter(request, response);
+                return;
+            } else { // Authorization가 없는 경우 메시지 출력(헤더 검증 실패처리)
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                // json 리턴 및 한글깨짐 수정.
+                response.setContentType("application/json;charset=utf-8");
+                JSONObject json = new JSONObject();
+                String message ="FAIL CHECK API TOKEN";
+                json.put("code", "403");
+                json.put("message", message);
+
+                PrintWriter out = response.getWriter();
+                out.print(json);
+                return;
+            }
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    /**
+     * Authorization 헤더 처리
+     * @param request
+     * @return
+     */
+    private boolean checkAuthHeader(HttpServletRequest request) {
+
+        boolean checkResult = false;
+
+        String authHeader = request.getHeader("Authorization");
+
+        if(StringUtils.hasText(authHeader)) {
+            log.info("Authorization header Value: " + authHeader);
+            if(authHeader.equals("12345678")) {
+                checkResult = true;
+            }
+        }
+
+        return checkResult;
     }
 }
