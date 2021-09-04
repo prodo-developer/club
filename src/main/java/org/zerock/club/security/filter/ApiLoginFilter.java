ba82ffa42e60063527f35c041adc56f33761c34e
prodo-developer <wjdals300@gmail.com>
2021-09-04 20:32:21 +0900

    feat : 인증 실패 처리 (AuthenticationFailureHandler) 추가

--- src/main/java/org/zerock/club/security/filter/ApiLoginFilter.java
+++ src/main/java/org/zerock/club/security/filter/ApiLoginFilter.java
@@ -1,11 +1,12 @@
 package org.zerock.club.security.filter;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,18 +19,44 @@ public class ApiLoginFilter extends AbstractAuthenticationProcessingFilter {
         super(defaultFilterProcessesUrl);
     }
 
+    /**
+     * email,pw를 받아서 실제 인증을 처리
+     * @param request
+     * @param response
+     * @return
+     * @throws AuthenticationException
+     * @throws IOException
+     * @throws ServletException
+     */
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+                                                throws AuthenticationException, IOException, ServletException {
         log.info("--------------ApiLoginFilter---------------");
         log.info("attemptAuthentication");
 
         String email = request.getParameter("email");
-        String pw = "1111";
+        String pw = request.getParameter("pw");
 
-        if(email == null) {
-            throw new BadCredentialsException("email cannot be null");
-        }
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(email, pw);
 
-        return null;
+        return getAuthenticationManager().authenticate(authToken);
+    }
+
+    /**
+     * 인증 성공 처리
+     * @param request
+     * @param response
+     * @param chain
+     * @param authResult
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        log.info("-------------ApiLoginFilter-----------");
+        log.info("successfulAuthentication: " + authResult);
+
+        log.info(String.valueOf(authResult.getPrincipal()));
     }
 }
