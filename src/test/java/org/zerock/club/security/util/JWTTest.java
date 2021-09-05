3e1431655da9ec6cffd569dcea656716cd5907d5
prodo-developer <wjdals300@gmail.com>
2021-09-05 20:15:00 +0900

    test : generateToken 토큰 검증

--- src/test/java/org/zerock/club/security/util/JWTTest.java
+++ src/test/java/org/zerock/club/security/util/JWTTest.java
@@ -1,6 +1,7 @@
 package org.zerock.club.security.util;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class JWTTest {
@@ -14,11 +15,26 @@ public class JWTTest {
     }
 
     @Test
+    @DisplayName("jwt토큰값 확인")
     public void testEncode() throws Exception {
         String email = "user95@zerock.co.kr";
 
-        String str = jwtUtil.generatetoken(email);
+        String str = jwtUtil.generateToken(email);
 
         System.out.println(str);
+    }
+
+    @Test
+    @DisplayName("generateToken 토큰 검증")
+    public void testValidate() throws Exception {
+
+        String email = "user95@zerock.co.kr";
+        String str = jwtUtil.generateToken(email);
+
+        Thread.sleep(5000);
+
+        String resultEmail = jwtUtil.validateAndExtract(str);
+
+        System.out.println(resultEmail);
     }
 }
